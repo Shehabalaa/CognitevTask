c59b6245c1da8d289fb6d29e8b1f582278e72e44
Shehab <Shehabalaa97@gmail.com>
2019-05-18 23:14:16 +0200

    Add acquire campaign from api middle ware if campaing is empty

---
+++ middlewares/acquireCampaign.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+module.exports = function (req, res, next) {
+    if (req.body.category != null && req.body.category == "") {
+        request.get('https://ngkc0vhbrl.execute-api.eu-west-1.amazonaws.com/api/?url=https://arabic.cnn.com/', { json: true }, async (err, res, body) => {
+            if (err) { return console.log(err); }
+            req.body.category = body.category.name;
+            next();
+        });
+        setInterval(next,1000); // Wait api or return error that campaing has to be filled
+    }else
+        next();
+    
+}
